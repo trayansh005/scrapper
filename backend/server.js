eac8a98e393e2f842abb73d5b80223cb4093cc93
Trayansh Agarwal <74540362+trayansh005@users.noreply.github.com>
2025-10-17 12:07:38 +0530

    fixed backend issues

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ const vm = require("vm");
 const app = express();
 
 // Enable CORS
-app.use(cors({
-	origin: "*",
-	methods: ["GET", "POST", "PUT", "DELETE"],
-	allowedHeaders: ["Content-Type"],
-}));
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type"],
+	})
+);
 
 app.use(express.json());
 
