356e703b028b21edcc0891355e08e267df857226
Trayansh Agarwal <74540362+trayansh005@users.noreply.github.com>
2026-03-02 16:45:03 +0530

    updated

--- backend/scraper-agent-75.js
+++ backend/scraper-agent-75.js
@@ -11,12 +11,7 @@ const {
 	updatePriceByPropertyURLOptimized,
 	processPropertyWithCoordinates,
 } = require("./lib/db-helpers.js");
-const {
-	extractCoordinatesFromHTML,
-	isSoldProperty,
-	parsePrice,
-	formatPriceDisplay,
-} = require("./lib/property-helpers.js");
+const { isSoldProperty, parsePrice } = require("./lib/property-helpers.js");
 const { createAgentLogger } = require("./lib/logger-helpers.js");
 const { blockNonEssentialResources } = require("./lib/scraper-utils.js");
 
